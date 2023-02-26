0d0cbdf8d0509288ad1810c14cf854d0eea0ccdd
“keyie” <570588655@qq.com>
2023-02-26 22:05:20 +0800

    vuex init

---
+++ vuex4/index.ts
@@ -0,0 +1,58 @@
+import { App, inject } from 'vue'
+
+const injectKey = "store"
+
+export function useStore<S>(): TinyStore<S> {
+  return inject(injectKey)
+}
+
+export function createStore<S>(options: StoreOptions<S>) {
+  return new TinyStore<S>(options)
+}
+
+class TinyStore<S = any> {
+  constructor(options: StoreOptions<S>) { }
+
+  install(app: App) {
+    app.provide(injectKey, this)
+  }
+}
+
+interface StoreOptions<S> {
+  state?: S,
+  getters?: GetterTree<S, S>,
+  mutations?: MutationTree<S>,
+  actions?: ActionTree<S, S>
+}
+
+// ActionTree
+interface ActionTree<S, R> {
+  [K: string]: Action<S, R>
+}
+
+// MutationTree
+interface MutationTree<S> {
+  [K: string]: Mutation<S>
+}
+
+// GetterTree
+interface GetterTree<S, R> {
+  [K: string]: Getter<S, R>
+}
+
+interface ActionContext<S, R> {
+  dispatch: Dispatch,
+  commit: Commit,
+  state: S
+}
+
+type Dispatch = (type: string, payload: any) => any
+type Commit = (type: string, payload: any) => any
+
+type Action<S, R> = (context: ActionContext<S, R>, payload?: any) => any
+
+type Mutation<S> = (state: S, payload?: any) => void
+
+type Getter<S, R> = (state: S, getters: any, rootState: R, rootGetters: any) => any
+
+export { }
