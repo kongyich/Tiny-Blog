b0eed30d5cd1693943431bf631c3929b3f4e3d92
884153724@qq.com <570588655@qq.com>
2023-03-18 23:55:24 +0800

    feat:add axios instance

--- Axios/ts-axios/src/types/index.ts
+++ Axios/ts-axios/src/types/index.ts
@@ -60,4 +60,5 @@ export interface Axios {
 
 export interface AxiosInstance extends Axios {
   (config: AxiosRequestConfig): AxiosPromise
+  (url: string, config?: AxiosRequestConfig): AxiosPromise
 }
