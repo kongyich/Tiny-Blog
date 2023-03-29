6aefd84d6444a55ed47edbf7fedd96aecf709012
884153724@qq.com <570588655@qq.com>
2023-03-29 23:28:00 +0800

    feat:react virtualDOM init

--- tiny-react-virtualDOM/src/TinyReact/createElement.js
+++ tiny-react-virtualDOM/src/TinyReact/createElement.js
@@ -1,7 +1,17 @@
 export default function createElement(type, props, ...children) {
+  const childElements = [].concat(children).reduce((res, cur) => {
+    if(cur !== true && cur !== false && cur !== null) {
+      if(cur instanceof Object) {
+        res.push(cur)
+      } else {
+        res.push(createElement('text', { textContent: cur }))
+      }
+    }
+    return res
+  }, [])
   return {
     type,
-    props,
-    children
+    props: Object.assign({ children: childElements }, props),
+    children: childElements
   }
 } 
