42bcedeb36c76769407a0d3b127756ce7aa59586
884153724@qq.com <570588655@qq.com>
2023-04-03 22:33:19 +0800

    feat:add difference diff

--- tiny-react-virtualDOM/src/TinyReact/diff.js
+++ tiny-react-virtualDOM/src/TinyReact/diff.js
@@ -1,11 +1,15 @@
 import mountElement from './mountElement'
 import updateElementNode from './updateElementNode'
 import updateTextNode from './updateTextNode'
+import createElement from './createElement'
 
 export default function diff(newVirtualDOM, container, oldDOM) {
 const oldVirtualDOM = oldDOM && oldDOM._virtualDOM
 if(!oldDOM) {
   mountElement(newVirtualDOM, container)
+} else if(oldVirtualDOM.type !== newVirtualDOM.type && typeof newVirtualDOM.type !== 'function') {
+  const newElement = createElement(virtualDOM)
+  oldDOM.parentNode.replaceChild(newElement, oldDOM)
 } else if(oldVirtualDOM && oldVirtualDOM.type === newVirtualDOM.type) {
     if(newVirtualDOM.type === 'text') {
       // 文本节点
