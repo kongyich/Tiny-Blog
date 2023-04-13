bf10bc3c363fa51ee5d0ccf517b771166c20d14d
884153724@qq.com <570588655@qq.com>
2023-04-13 23:19:49 +0800

    feat: add reconciliation

--- react-fiber/src/react/misc/createTaskQueue/index.js
+++ react-fiber/src/react/misc/createTaskQueue/index.js
@@ -2,8 +2,20 @@ const createTaskQueue = () => {
   const taskQueue = []
 
   return {
+    /**
+     * 向任务队列添加任务
+    */
     push: item => taskQueue.push(item),
-    pop: () => taskQueue.shift()
+    /**
+     * 从任务队列获取任务
+    */
+    pop: () => taskQueue.shift(),
+
+    /**
+     * 
+     * 判断是否还有任务？
+    */
+   isEmpty: () => taskQueue.length === 0
   }
 }
 
--- react-fiber/src/react/reconciliation/index.js
+++ react-fiber/src/react/reconciliation/index.js
@@ -2,6 +2,39 @@ import { createTaskQueue } from "../misc"
 
 const taskQueue = createTaskQueue()
 
+const subTask = null
+
+const getFirstTask = () => {
+
+}
+
+const executeTask = fiber => {}
+
+const workLoop = deadline => {
+  if(!subTask) {
+    subTask = getFirstTask()
+  }
+
+  /**
+   * 任务存在并且浏览器有空余时间
+   * 执行任务/接受任务/返回新的任务
+  */
+
+  while(subTask && deadline.timeRemaining() > 1) {
+    subTask = executeTask(subTask)
+  }
+}
+
+const performTask = deadline => {
+  workLoop(deadline)
+  /**
+   * 是否还有没执行的任务？
+  */
+  if(subTask || !taskQueue.isEmpty()) {
+    requestIdleCallback(performTask)
+  }
+}
+
 export const render = (element, dom) => {
 /**
  * 1. 向任务队列中添加任务
@@ -10,8 +43,14 @@ export const render = (element, dom) => {
  * 通过 vdom 对象，构建fiber对象
 */
 
-taskQueue.push({
-  dom,
-  props: { children: element }
-})
+  taskQueue.push({
+    dom,
+    props: { children: element }
+  })
+
+  /**
+   * 在浏览器空闲的时候执行任务
+  */
+
+  requestIdleCallback(performTask)
 }
