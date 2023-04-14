27cc35e3d825d00162624c99e7e6356d60a55e5e
884153724@qq.com <570588655@qq.com>
2023-04-14 23:35:41 +0800

    feat: add root fiber

--- react-fiber/src/react/reconciliation/index.js
+++ react-fiber/src/react/reconciliation/index.js
@@ -5,7 +5,23 @@ const taskQueue = createTaskQueue()
 const subTask = null
 
 const getFirstTask = () => {
+  /**
+   * 从任务队列中获取任务
+  */
 
+  const task = taskQueue.pop()
+
+  /**
+   * 返回最外层节点的fiber对象
+  */
+
+  return {
+    props: task.props,
+    stateNode: task.dom,
+    tag: "host_root",
+    effects: [],
+    child: null
+  }
 }
 
 const executeTask = fiber => {}
