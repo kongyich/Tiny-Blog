044f29a6a127c7c625f328c2bf95d61bf1d408e5
884153724@qq.com <570588655@qq.com>
2023-05-25 23:08:39 +0800

    feat: add middleware

--- redux/redux-toolkit/src/Store/index.js
+++ redux/redux-toolkit/src/Store/index.js
@@ -1,9 +1,11 @@
-import { configureStore } from "@reduxjs/toolkit"
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit"
 import TodosReducer, { TODOS_FEATURE_KEY } from "./todos.slice"
+import logger from 'redux-logger'
 
 export default configureStore({
   reducer: {
     [TODOS_FEATURE_KEY]: TodosReducer
   },
   devTools: process.env.NODE_ENV !== "production",
+  middleware: [...getDefaultMiddleware(), logger]
 })
